2d4d323909dac3c58f8db125271dc5651518d52b
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-22 16:51:39 -0700

    Add SelectionTopCenterPositioner for annotate.

--- src/annotate/index.ts
+++ src/annotate/index.ts
@@ -1,6 +1,7 @@
 import * as rangeUtils from './utils/range';
 
 export { SelectionObserver } from './selection-observer';
+export { SelectionTopCenterPositioner } from './tooltip/positioner';
 export {
     rangeUtils,
 };
