34283393265e98712f454525cdb00e70b8ca68cb
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-22 17:22:45 -0700

    Remove anchor export.

--- src/annotate/index.ts
+++ src/annotate/index.ts
@@ -1,5 +1,6 @@
 import * as rangeUtils from './utils/range';
 
+export { RangeAnchor } from './anchor/types';
 export { SelectionObserver } from './selection-observer';
 export { SelectionTopCenterPositioner } from './tooltip/positioner';
 export {
